6a3c4aa9b47f1dd676b2a22ee1d5bd4c879bdfc1
Elabyad247 <elabyad247@gmail.com>
2025-03-30 00:43:14 +0200

    Add layout structure with header, footer, and content area

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import "./App.css";
 import Sidebar from "./components/Sidebar";
-// import ColorPalettes from "./ColorPalettes";
+import ColorPalettes from "./ColorPalettes";
 
 function App() {
   return (
-    <>
+    <div className="app">
       <Sidebar />
-    </>
+      <div className="right-area">
+        <div className="app-header">
+          <h1>App Header</h1>
+        </div>
+        <div className="app-content">
+          <p>App Content</p>
+          <ColorPalettes />
+        </div>
+        <div className="app-footer">
+          <p>App Footer</p>
+        </div>
+      </div>
+    </div>
   );
 }
 
